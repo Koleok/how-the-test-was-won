5823dc6ab00b9991c12d5d383ab8b1b6d50ab5d9
Koleok <kchamb3@gmail.com>
2016-04-15 10:32:10 -0400

    Clarify testIfExists reporting and bump version

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ chai.use(asPromised)
 const primitiveEquals = curry(
   (type, val) => {
     it(`should be an ${type}`, () => {
-      expect(val).to.be.an(type)
+      expect(val)
+        .to.be.an(type)
     })
   }
 )
@@ -33,30 +34,34 @@ export const shouldBeAFunction = primitiveEquals('function')
  * @return {undefined}
  */
 export function testIfExists(val, async) {
-  describe('should not return a falsey value', () => {
-    it('not null', () => {
-      if (async) {
-        expect(val).to.eventually.not.be.null
-      } else {
-        expect(val).to.not.be.null
-      }
-    })
+  it('should not be null', () => {
+    if (async) {
+      expect(val)
+        .to.eventually.not.be.null
+    } else {
+      expect(val)
+        .to.not.be.null
+    }
+  })
 
-    it('not undefined', () => {
-      if (async) {
-        expect(val).to.eventually.not.be.undefined
-      } else {
-        expect(val).to.not.be.undefined
-      }
-    })
+  it('should not be undefined', () => {
+    if (async) {
+      expect(val)
+        .to.eventually.not.be.undefined
+    } else {
+      expect(val)
+        .to.not.be.undefined
+    }
+  })
 
-    it('not an empty object or array', () => {
-      if (async) {
-        expect(val).to.eventually.not.be.empty
-      } else {
-        expect(val).to.not.be.empty
-      }
-    })
+  it('should not be an empty object or array', () => {
+    if (async) {
+      expect(val)
+        .to.eventually.not.be.empty
+    } else {
+      expect(val)
+        .to.not.be.empty
+    }
   })
 }
 
